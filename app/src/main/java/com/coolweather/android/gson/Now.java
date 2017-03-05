e253b4737ee75069a08ed4705dd63b41d04486ce
Naughoy <18768755688@163.com>
2017-03-05 22:14:38 +0800

    加入天气信息功能

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,22 @@
+package com.coolweather.android.gson;
+
+/*当前天气信息     */
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
